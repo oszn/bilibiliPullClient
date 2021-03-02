901c1b6816accc9e8f7fdfd2830b4d297b869898
oszn <1091756452@qq.com>
2021-03-02 17:37:47 +0800

    create dev branch

--- src/main/java/com/myLogger/http/walkMilk.java
+++ src/main/java/com/myLogger/http/walkMilk.java
@@ -3,10 +3,7 @@ package com.myLogger.http;
 import com.alibaba.fastjson.JSONObject;
 import com.myLogger.component.tools.json2map;
 import com.myLogger.consist.ErrorEnum;
-import com.myLogger.dao.entry.Bili;
-import com.myLogger.dao.entry.Bilibili;
-import com.myLogger.dao.entry.Bilibilistep;
-import com.myLogger.dao.entry.Errorlog;
+import com.myLogger.dao.entry.*;
 import com.myLogger.dao.mapper.BiliMapper;
 import com.myLogger.dao.mapper.BilibiliMapper;
 import com.myLogger.dao.mapper.BilibilistepMapper;
@@ -19,6 +16,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+//import java.lang.reflect.Proxy;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.SocketTimeoutException;
+import java.security.PublicKey;
 import java.util.Date;
 
 @Service
@@ -56,6 +58,28 @@ public class walkMilk {
         }
         return null;
     }
+    public ResponseBody doFetchVersion2(String uid){
+        OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
+        IpProxy ipProxy=getIpProxy();
+        Proxy proxy=new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ipProxy.getIp(), ipProxy.getPort()));
+        builder.proxy(proxy);
+        OkHttpClient client=builder.build();
+        Request request = new Request.Builder()
+                .url("http://api.bilibili.com/x/web-interface/card?mid="+uid+"&photo=true")
+                .method("GET", null)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36")
+                .addHeader("Cookie", "mid=xxx; bfe_id=1bad38f44e358ca77469025e0405c4a6")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
     public void logfile(String key,String log){
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
@@ -74,6 +98,28 @@ public class walkMilk {
             e.printStackTrace();
         }
 
+    }
+    public IpProxy getIpProxy(){
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://42.193.170.22:4003/get/")
+                .method("GET", null)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String body=response.body().string();
+            JSONObject jsonObject=JSONObject.parseObject(body);
+            IpProxy ipProxy=new IpProxy();
+//            ipProxy.setIp(jsonObject.getString());
+            String []ipandPort=jsonObject.getString("proxy").split(":");
+            ipProxy.setIp(ipandPort[0]);
+            ipProxy.setPort(Integer.parseInt(ipandPort[1]));
+            return ipProxy;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  null;
     }
     public int mul(int limit){
 //        System.out.println("?");
@@ -112,14 +158,58 @@ public class walkMilk {
         errorlog.setType(type);
         errorlogMapper.insert(errorlog);
     }
+    public String selectUid(String uid,String epoch,Bilibilistep c){
 
+        if(uid=="null"){
 
+            uid= String.valueOf(c.getValue());
+        }
+        return uid;
+    }
+
+    public String pushServer(String ans,String uid,Bilibilistep c){
+
+        Bili bilibili = new Bili();
+        bilibili.setMac(n);
+        bilibili.setInsertDate(new Date(System.currentTimeMillis()));
+
+        j2m.init(ans,bilibili);
+
+        bilibiliMapper.insertSelective(bilibili);
+        c.setValue(c.getValue()+1);
+        bilibilistepMapper.updateByPrimaryKey(c);
+        return uid;
+    }
+    public String pushVersion2(String uid,String epoch){
+        Bilibilistep c=bilibilistepMapper.selectByPrimaryKey(epoch);
+        uid=selectUid(uid,epoch,c);
+        String ans=new String();
+        try {
+            ans=doFetchVersion2(uid).string();
+
+        } catch (IOException e) {
+            eLog(uid,ErrorEnum.FETCH_ERR.type(),e);
+            e.printStackTrace();
+        }
+        Bili bilibili = new Bili();
+        bilibili.setMac(n);
+        bilibili.setInsertDate(new Date(System.currentTimeMillis()));
+
+        j2m.init(ans,bilibili);
+
+        bilibiliMapper.insertSelective(bilibili);
+        c.setValue(c.getValue()+1);
+        bilibilistepMapper.updateByPrimaryKey(c);
+
+        return uid;
+    }
     public String insert(String uid,String epoch){
         Bilibilistep c=bilibilistepMapper.selectByPrimaryKey(epoch);
         if(uid=="null"){
 
             uid= String.valueOf(c.getValue());
         }
+
         String ans=new String();
         try {
             ans=doFetch(uid).string();
@@ -141,7 +231,23 @@ public class walkMilk {
 
         return uid;
     }
-    public static void main(String[] args) {
+    public void tf2(){
+        walkMilk w=new walkMilk();
+        int k=1;
+        while (true){
+            try {
+                String ans=w.doFetchVersion2(String.valueOf(k)).string();
+                System.out.println(ans);
+                Thread.sleep(100);
+            } catch (Exception e ) {
+                e.printStackTrace();
+            }
+            k+=1;
 
+        }
+    }
+    public static void main(String[] args) {
+        walkMilk w=new walkMilk();
+        w.tf2();
     }
 }
